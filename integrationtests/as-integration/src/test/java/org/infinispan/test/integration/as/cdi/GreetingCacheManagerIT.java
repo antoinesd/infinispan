44f55472d7be0694cdeed314844b4835721b4156
Tristan Tarrant <ttarrant@redhat.com>
2014-03-14 09:37:22 +0100

    ISPN-4129 Inlcude Query, CDI, JCache and the LevelDB and REST cachestores to the AS modules

--- integrationtests/as-integration/src/test/java/org/infinispan/test/integration/as/cdi/GreetingCacheManagerIT.java
+++ integrationtests/as-integration/src/test/java/org/infinispan/test/integration/as/cdi/GreetingCacheManagerIT.java
@@ -1,29 +1,43 @@
 package org.infinispan.test.integration.as.cdi;
 
-import org.infinispan.eviction.EvictionStrategy;
-import org.infinispan.test.integration.as.category.UnstableTest;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
 import javax.inject.Inject;
 
-import static org.junit.Assert.*;
+import org.infinispan.Version;
+import org.infinispan.eviction.EvictionStrategy;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.spec.se.manifest.ManifestDescriptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Kevin Pollet <pollet.kevin@gmail.com> (C) 2011
  */
 @RunWith(Arquillian.class)
-@Category(UnstableTest.class)
 public class GreetingCacheManagerIT {
 
    @Deployment
-   public static WebArchive deployment() {
-      return Deployments.baseDeployment()
-            .addClass(GreetingCacheManagerIT.class);
+   public static Archive<?> deployment() {
+      WebArchive archive = ShrinkWrap
+            .create(WebArchive.class, "cdi-cm.war")
+            .addPackage(GreetingCacheManagerIT.class.getPackage())
+            .add(manifest(), "META-INF/MANIFEST.MF")
+            .addAsWebInfResource("beans.xml");
+      return archive;
+   }
+
+   private static Asset manifest() {
+      String manifest = Descriptors.create(ManifestDescriptor.class)
+            .attribute("Dependencies", "org.infinispan.cdi:" + Version.MODULE_SLOT + " services, org.infinispan.jcache:" + Version.MODULE_SLOT +" services").exportAsString();
+      return new StringAsset(manifest);
    }
 
    @Inject
